f86023eb01b6bc715aa3f0188d701da381154a22
Sean Oughton <seanoughton@gmail.com>
2018-06-27 22:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const totalRevenue = function (drivers){
 const averageRevenue = function(drivers){
   const average = function (agg,el,i,arr){
     total = agg.total + el.revenue
+    avg = total
     return {
       //avg = total/drivers.length;
       //total
