62e1ad8db90dfcdd159f6b36d70b00bd18961d75
Sean Oughton <seanoughton@gmail.com>
2018-06-27 21:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,7 @@ const logDriversByHometown = function (drivers,location) {
 
 //driversByRevenue() — Receives an array of driver objects and returns a new array of driver objects sorted by their revenue attribute from lowest to highest.
 
+
+const driversByRevenue = function (drivers) {
+  
+}
