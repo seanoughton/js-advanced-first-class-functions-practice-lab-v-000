48fe9839a068888041ce577d88d837b04954b922
Sean Oughton <seanoughton@gmail.com>
2018-06-27 22:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,5 +52,11 @@ const totalRevenue = function (drivers){
 // averageRevenue() — Receives an array of driver objects and returns the average revenue earned by each driver.
 
 const averageRevenue = function(drivers){
-  
+  const avgRevenue = function (agg,el,i,arr){
+    total = agg.total + el.revenue
+    return {
+      total
+    }
+  };
+  return drivers.reduce(sumRevenue,{total:0}).total;
 }
