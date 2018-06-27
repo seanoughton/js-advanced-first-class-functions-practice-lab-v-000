b20041df9e96a798ae41e0780c0d46dd95ce3a4c
Sean Oughton <seanoughton@gmail.com>
2018-06-27 21:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solution in this file!
+
+//Receives an array of driver objects and logs the name attribute of each driver to the console.
 const logDriverNames = function (drivers) {
   const getNames = function (el,i,arr) {
     console.log(el.name);
   };
   return drivers.forEach(getNames);
 }
+
+//logDriversByHometown() — Receives an array of driver objects as the first argument and a location as the second argument. The function logs to the console the
