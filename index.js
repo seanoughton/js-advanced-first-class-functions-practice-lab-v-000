5d8c312df338be654893533a3b6ef99269a209d4
Sean Oughton <seanoughton@gmail.com>
2018-06-27 22:37:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const driversByName = function (drivers){
 //totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
 const totalRevenue = function (drivers){
   const sumRevenue = function (agg,el,i,arr){
-    let sum = agg.revenue + el.revenue
+    let sum = agg + el.revenue
     return sum
   };
   drivers.reduce(sumRevenue);
