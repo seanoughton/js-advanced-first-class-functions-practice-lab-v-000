375cb8955d2e56195b0312ae66ff5fb38168682b
Sean Oughton <seanoughton@gmail.com>
2018-06-27 22:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,5 +32,9 @@ const driversByRevenue = function (drivers) {
 //driversByName() — Receives an array of driver objects and returns a new array of driver objects sorted by their name attribute from A to Z. Here, you may have to use the String.prototype.localeCompare() method.
 
 const driversByName = function (drivers){
+  const comparator = function (a,b) {
+    return a.localeCompare.b;
+  };
+  return drivers.concat().sort(comparator);
 
 }
