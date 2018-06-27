f7e4c9173cedf0ba2cf31b51fa168f502ef05339
Sean Oughton <seanoughton@gmail.com>
2018-06-27 21:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solution in this file!
 var logDriverNames = function (drivers) {
   //Receives an array of driver objects and logs the name attribute of each driver to the console.
+  const getNames = function (el,i,arr) {
+    
+  }
   return drivers.for_each(getNames);
 }
